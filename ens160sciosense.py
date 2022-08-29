29135d5fb7e6f9663c24538e606ca79f48124466
octaprog7 <goctaprog@gmail.com>
2022-08-29 13:50:53 +0500

    + get_firmware_version method + more code description

--- ens160sciosense.py
+++ ens160sciosense.py
@@ -4,14 +4,7 @@
 
 import micropython
 from sensor_pack import bus_service
-from sensor_pack.base_sensor import BaseSensor, Iterator
-
-
-@micropython.native
-def _check_value(value: int, valid_range, error_msg: str) -> int:
-    if value not in valid_range:
-        raise ValueError(error_msg)
-    return value
+from sensor_pack.base_sensor import BaseSensor, Iterator, check_value
 
 
 class Ens160(BaseSensor, Iterator):
@@ -53,7 +46,8 @@ class Ens160(BaseSensor, Iterator):
                 0x01:   IDLE mode (low power)
                 0x02:   STANDARD Gas Sensing Mode
                 0xF0:   RESET"""
-        self._write_register(0x10, new_mode, 1)
+        nm = check_value(new_mode, range(3))
+        self._write_register(0x10, nm, 1)
 
     def get_mode(self) -> int:
         """Return current operation mode of sensor"""
@@ -61,35 +55,46 @@ class Ens160(BaseSensor, Iterator):
         return self.unpack("B", reg_val)[0]
 
     def get_config(self) -> int:
+        """Return current config from sensor.
+        Pls see Table 19: Register CONFIG in official documentation!"""
         reg_val = self._read_register(0x11, 1)
         return self.unpack("b", reg_val)[0]
 
     def set_config(self, new_config: int) -> int:
+        """Set current config sensor.
+        Pls see Table 19: Register CONFIG in official documentation!"""
         return self._write_register(0x11, new_config, 1)
 
     def _exec_cmd(self, cmd: int) -> bytes:
-        _check_value(cmd, (0x00, 0x0E, 0xCC), f"Invalid command code: {cmd}")
+        check_value(cmd, (0x00, 0x0E, 0xCC), f"Invalid command code: {cmd}")
         self._write_register(0x12, cmd, 1)
-        # read General Purpose WRITE Registers at address 0x40-47
-        return self._read_register(0x40, 8)
+        return self._read_register(0x48, 8)
 
-    def set_ambient_temp(self, value: float):
-        """write ambient temperature data to ENS160 for compensation. value in Celsius"""
-        t = int(64*(273.15+value))
+    def set_ambient_temp(self, value_in_celsius: float):
+        """write ambient temperature data to ENS160 for compensation. value in Celsius.
+        This value must be read from the temperature sensor! It must be correct!
+        Записывает в ENS160 датчик температуру окружающей среды, для компенсации!
+        Значение в градусах Цельсия! Это значение должно быть считано с датчика температуры!
+        Это должно быть правильным!
+        """
+        t = int(64*(273.15+value_in_celsius))
         self._write_register(0x13, t, 2)
 
-    def set_humidity(self, value: float):
-        """write relative humidity data to ENS160 for compensation. value in percent"""
-        h = int(512*value)
-        self._write_register(0x15, h, 2)
+    def set_humidity(self, rel_hum: int):
+        """write relative humidity data to ENS160 for compensation. value in percent.
+        Записывает в ENS160 датчик относительную влажность, для компенсации! значение в процентах!"""
+        check_value(rel_hum, range(101), f"Invalid command code: {rel_hum}")
+        self._write_register(0x15, rel_hum << 9, 2)
 
     def get_status(self) -> int:
-        """indicates the current status of the ENS160"""
+        """indicates the current status of the ENS160.
+        Возвращает текущий статус ENS160 в виде байта"""
         reg_val = self._read_register(0x20, 1)
         return self.unpack("b", reg_val)[0]
 
     def get_air_quality_index(self) -> int:
-        """reports the calculated Air Quality Index according to the UBA"""
+        """reports the calculated Air Quality Index according to the UBA.
+        Возвращает расчетный индекс качества воздуха в соответствии с UBA"""
         reg_val = self._read_register(0x21, 1)
         return self.unpack("b", reg_val)[0] & 0x07
 
@@ -110,11 +115,11 @@ class Ens160(BaseSensor, Iterator):
         reg_val = self._read_register(0x38, 1)
         return self.unpack("b", reg_val)[0]
 
-
-    def get_illumination(self) -> int:
-        """Return illumination in lux"""
-        tmp = self.adapter.read(self.address, 2)
-        return self.unpack("H", tmp)[0]     # .unpack(">H", tmp)[0])
+    def get_firmware_version(self) -> tuple:
+        """Return the firmware version of the ENS160 as tuple (Major, Minor, Release).
+        Возвращает версию прошивки ENS160 в виде кортежа (Major, Minor, Release)"""
+        b = self._exec_cmd(0x0E)
+        return b[4], b[5], b[6]
 
     def __next__(self) -> int:
-        return self.get_illumination()
+        pass
